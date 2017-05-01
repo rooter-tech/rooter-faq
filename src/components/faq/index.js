5bd9a7d54cd7b17d7122e326e1189e5e1c269f39
Divyanshu Tomar <tomar.divyanshu@gmail.com>
2017-05-01 21:00:26 +0530

    moved json data to s3

--- src/components/faq/index.js
+++ src/components/faq/index.js
@@ -1,20 +1,9 @@
 import { h, Component } from 'preact';
 import lunr from 'lunr';
 import _ from 'lodash';
-import faqData from '../../assets/faqData.json';
 import SectionContainer from './SectionContainer';
 
-const idx = lunr(function () {
-	this.ref('id');
-	this.field('question');
-	this.field('answer');
-	faqData.forEach(function (qa) {
-		this.add(qa);
-	}, this);
-});
-const faqByKey = _.keyBy(faqData, 'id');
-
-const computeFilteredFAQ = (matchedIDs, searchStr) => {
+const computeFilteredFAQ = (faqData,faqByKey,matchedIDs, searchStr) => {
 	if (searchStr && searchStr.length > 3) {
 		if (matchedIDs.length > 0) {
 			return matchedIDs.map((qaID) => faqByKey[qaID]);
@@ -28,38 +17,82 @@ export default class FAQ extends Component {
 	constructor (props) {
 		super(props);
 		this.state = {
-			faqs: faqData,
+			isLoadingFaqs: true,
+			errorMessage: '',
+			allFaqs: undefined,
+			faqsByKey: undefined,
+			filteredFaqs: undefined,
 			searchQuery: ''
 		};
 		_.bindAll(this, ['onChangeSearchText']);
 	}
+	componentDidMount() {
+		fetch("https://s3-ap-southeast-1.amazonaws.com/rooter-faq/faqData.json")
+		.then((response) => response.json())
+		.then((data) => {
+			this.setState(() => {
+				return {
+					isLoadingFaqs: false,
+					errorMessage: '',
+					allFaqs: data,
+					faqsByKey: _.keyBy(data, 'id'),
+					filteredFaqs: data,
+					searchQuery: ''
+				};
+			});
+			this.idx = lunr(function () {
+				this.ref('id');
+				this.field('question');
+				this.field('answer');
+				data.forEach(function (qa) {
+					this.add(qa);
+				}, this);
+			});
+		})
+		.catch(() => {
+			this.setState({
+				errorMessage: 'Some error occured',
+				isLoadingFaqs: false
+			});
+		});
+	}
 	onChangeSearchText(event) {
 		const newStr = event.target.value;
-		this.setState(() => {
+		this.setState((prevState) => {
 			return {
 				searchQuery : newStr,
-				faqs: computeFilteredFAQ(idx.search(newStr).map(item=>item.ref), newStr)
+				filteredFaqs: computeFilteredFAQ(prevState.allFaqs,prevState.faqsByKey,this.idx.search(newStr).map(item=>item.ref), newStr)
 			};
 		});
 	}
 	render(props, state) {
-		const groupedFaqs = _.groupBy(state.faqs, 'type');
+		const groupedFaqs = state.filteredFaqs ? _.groupBy(state.filteredFaqs, 'type') : undefined;
 		return (
-			<div>
-				<section className="section">
-					<div className="container">
-						<p class="control has-icons-left">
-							<input value={state.searchQuery} className="input" type="text" placeholder="Search..." onInput={this.onChangeSearchText} />
-							<span class="icon is-small is-left">
-							<i class="fa fa-search" />
-							</span>
-						</p>
-					</div>
-				</section>
-				<SectionContainer queryStr={state.searchQuery} sectionType='Prizes' containerData={groupedFaqs.prize || []} />
-				<SectionContainer queryStr={state.searchQuery} sectionType='Redeem and Refund' containerData={groupedFaqs.redeem_refund || []} />
-				<SectionContainer queryStr={state.searchQuery} sectionType='Tech Support' containerData={groupedFaqs.tech_support || []} />
-			</div>
+			state.isLoadingFaqs ?
+				<h4 style={{ textAlign: 'center' }}>Loading...</h4>
+				:
+				<div>
+					{
+						groupedFaqs && !state.errorMessage ?
+							<div>
+								<section className="section">
+									<div className="container">
+										<p class="control has-icons-left">
+											<input value={state.searchQuery} className="input" type="text" placeholder="Search..." onInput={this.onChangeSearchText} />
+											<span class="icon is-small is-left">
+											<i class="fa fa-search" />
+											</span>
+										</p>
+									</div>
+								</section>
+								<SectionContainer queryStr={state.searchQuery} sectionType='Prizes' containerData={groupedFaqs.prize || []} />
+								<SectionContainer queryStr={state.searchQuery} sectionType='Redeem and Refund' containerData={groupedFaqs.redeem_refund || []} />
+								<SectionContainer queryStr={state.searchQuery} sectionType='Tech Support' containerData={groupedFaqs.tech_support || []} />
+							</div>
+						:
+						<h3 style={{ color: 'red', textAlign: 'center' }}>{state.errorMessage}</h3>
+					}
+				</div>
 		);
 	}
 }
--- src/components/rules/index.js
+++ src/components/rules/index.js
@@ -1,14 +1,50 @@
 import { h, Component } from 'preact';
 import RulesContainer from './RulesContainer';
-import rulesData from '../../assets/rulesData.json';
 
 export default class Rules extends Component {
-	render() {
+	constructor(props) {
+		super(props);
+		this.state = {
+			rulesData: undefined,
+			isLoadingRules: true,
+			errorMessage: ''
+		};
+	}
+	componentDidMount() {
+		fetch("https://s3-ap-southeast-1.amazonaws.com/rooter-faq/rulesData.json")
+		.then((response) => response.json())
+		.then((data) => {
+			this.setState(() => {
+				return {
+					isLoadingRules: false,
+					errorMessage: '',
+					rulesData: data
+				};
+			});
+		})
+		.catch(() => {
+			this.setState({
+				errorMessage: 'Some error occured',
+				isLoadingRules: false
+			});
+		});
+	}
+	render(props,state) {
 		return (
-			<div>
-				<RulesContainer rulesType='General' containerData={rulesData.general} />
-				<RulesContainer rulesType='Cricket' containerData={rulesData.cricket} />
-			</div>
+			state.isLoadingFaqs ?
+				<h4 style={{ textAlign: 'center' }}>Loading...</h4>
+				:
+				<div>
+					{
+						state.rulesData && !state.errorMessage ?
+							<div>
+								<RulesContainer rulesType='General' containerData={state.rulesData.general} />
+								<RulesContainer rulesType='Cricket' containerData={state.rulesData.cricket} />
+							</div>
+						:
+						<h3 style={{ color: 'red', textAlign: 'center' }}>{state.errorMessage}</h3>
+					}
+				</div>
 		);
 	}
 }
