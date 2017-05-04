85b23133eba7530c98f3f0d9fdea6ebd30446b83
Divyanshu Tomar <tomar.divyanshu@gmail.com>
2017-05-04 14:09:20 +0530

    added report button

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,12 +6,20 @@ export default class Header extends Component {
 		return (
 			<header>
 				<nav className='nav'>
-					<div class="nav-left" />
-					<div class="nav-center is-centered">
+					<div class="nav-left">
 						<Link href="/faq" className='nav-item'>FAQ</Link>
 						<Link href="/rules" className='nav-item'>Rules</Link>
 					</div>
-					<div class="nav-right" />
+					<div class="nav-right">
+						<div class='nav-item'>
+								<a class="button is-danger" href='mailto:support@rooter.io'>
+									<span>Report</span>
+									<span class="icon" style={{ marginRight: '0px' }}>
+										<i class="fa fa-bug" />
+									</span>
+								</a>
+						</div>
+					</div>
 				</nav>
 			</header>
 		);
