ce13efebce2e2e2617f058eb6e58da258972abdc
Divyanshu Tomar <tomar.divyanshu@gmail.com>
2017-09-08 21:42:39 +0530

    updated server port config

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const compress = require('compression');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 // Apply gzip compression
 app.use(compress());
@@ -18,6 +18,6 @@ app.listen(port, (err) => {
 	if (err) {
 		console.log('Error in firing up the server !');
 	}
-	console.log(`Server started...`);
+	console.log(`Server started at ${port}...`);
 });
 
