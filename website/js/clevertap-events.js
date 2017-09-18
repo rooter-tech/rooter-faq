72cd91bd3dccb6c5e44e3997635ce656d622e9b8
Divyanshu Tomar <tomar.divyanshu@gmail.com>
2017-09-18 17:20:58 +0530

    fixed ct events bug and added GA

--- website/js/clevertap-events.js
+++ website/js/clevertap-events.js
@@ -8,15 +8,33 @@ $(function() {
     // Logs to CT when user lands on static website
     logCTevent('Static_landing');
     // Logs to CT when user clicks on login
-    $('.login-wap, #mobile_login').click(logCTevent('Static_login'));
+    $('.login-wap, #mobile_login').click(function() {
+		logCTevent('Static_login');
+	});
     // Logs to CT on click of social icons in footer
-    $('a[href="https://www.facebook.com/hellorooter/"]').click(logCTevent('Social', { Platform: 'Facebook' }));
-    $('a[href="https://twitter.com/HelloRooter"]').click(logCTevent('Social', { Platform: 'Twitter' }));
-    $('a[href="https://www.linkedin.com/company-beta/17969705/"]').click(logCTevent('Social', { Platform: 'LinkedIn' }));
-    $('a[href="https://www.youtube.com/watch?v=42aaDrmY4Zc"]').click(logCTevent('Social', { Platform: 'Youtube' }));
+    $('a[href="https://www.facebook.com/hellorooter/"]').click(function() {
+		logCTevent('Social', { Platform: 'Facebook' });
+	});
+    $('a[href="https://twitter.com/HelloRooter"]').click(function() {
+		logCTevent('Social', { Platform: 'Twitter' });
+	});
+    $('a[href="https://www.linkedin.com/company-beta/17969705/"]').click(function() {
+		logCTevent('Social', { Platform: 'LinkedIn' });
+	});
+    $('a[href="https://www.youtube.com/watch?v=42aaDrmY4Zc"]').click(function() {
+		logCTevent('Social', { Platform: 'Youtube' });
+	});
     // Logs to CT for various download app buttons on the website
-    $('.get-app').click(logCTevent('Download_app', { Navigation: 'Static_home_page_header' }));
-    $('.feature-get-app').click(logCTevent('Download_app', { Navigation: 'Static_home_page_CSV' }));
-    $('.centralDLCTA').click(logCTevent('Download_app', { Navigation: 'Static_home_page_video' }));
-    $('.footerDLCTA').click(logCTevent('Download_app', { Navigation: 'Static_home_page_footer' }));
+    $('.get-app').click(function() {
+		logCTevent('Download_app', { Navigation: 'Static_home_page_header' });
+	});
+    $('.feature-get-app').click(function() {
+		logCTevent('Download_app', { Navigation: 'Static_home_page_CSV' });
+	});
+    $('.centralDLCTA').click(function() {
+		logCTevent('Download_app', { Navigation: 'Static_home_page_video' });
+	});
+    $('.footerDLCTA').click(function() {
+		logCTevent('Download_app', { Navigation: 'Static_home_page_footer' });
+	});
 })
