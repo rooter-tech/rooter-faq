231567198532dbcb755d06180f0e61ab829492bc
Divyanshu Tomar <tomar.divyanshu@gmail.com>
2017-11-04 13:23:58 +0530

    added forceDomain

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const compress = require('compression');
-
+const forceDomain = require('forcedomain');
 const app = express();
 const port = process.env.PORT || 8080;
 
 // Apply gzip compression
 app.use(compress());
-
+app.use(forceDomain({
+	hostname: 'www.rooter.io',
+	protocol: 'https'
+}));
 app.use(express.static('build'));
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/build/index.html');
 });
 app.get('/web/*', (req, res) => {
 	res.sendFile(__dirname + '/build/web/index.html');
-});
-app.get('/news/*', (req, res) => {
-	res.sendFile(__dirname + '/build/news/index.html');
 });
 app.listen(port, (err) => {
 	if (err) {
